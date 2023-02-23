214e3afb349ba9ecca69f8c460d59eb43bf6c4f6
adrianxmp <adrian.ponce@ucalgary.ca>
2023-02-23 01:37:23 -0700

    Added functions for the invoice lists

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from docxtpl import DocxTemplate
 
 doc = DocxTemplate("invoice_template.docx")
 
-
-doc.render({"name": "Patrick"})
+invoice_list = [[2, "pen", 0.5, 1]]
+doc.render({"name": "Patrick", "invoice_list": invoice_list})
 doc.save("new_invoice.docx")
